d2f2b27ec911efd415d90ffb4ac00bc1e3c30cf5
Tait Hoyem <tait@tait.tech>
2026-05-22 15:14:04 -0600

    Remove target files

--- consistent-api/target/debug/consistent-api.d
+++
@@ -1,1 +0,0 @@
-/home/tait/Documents/at-api-consistency/consistent-api/target/debug/consistent-api: /home/tait/Documents/at-api-consistency/consistent-api/src/main.rs
--- consistent-api/target/debug/deps/consistent_api-db6dc9f3e859565f.d
+++
@@ -1,5 +0,0 @@
-/home/tait/Documents/at-api-consistency/consistent-api/target/debug/deps/consistent_api-db6dc9f3e859565f.d: src/main.rs
-
-/home/tait/Documents/at-api-consistency/consistent-api/target/debug/deps/consistent_api-db6dc9f3e859565f: src/main.rs
-
-src/main.rs:
